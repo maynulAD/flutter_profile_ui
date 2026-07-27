e1f33f54b1d4a0fe88f3f21850e96635f3d00315
Md Gazi Maynul Hassan Moin <maynulhassan127155@gmail.com>
2026-07-27 20:41:36 +0600

    Refactor main.dart to implement ContactListPage
    Refactored MyApp to use ContactListPage and removed unused widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -7,48 +7,105 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 252, 200, 45),
-          elevation: 0,
-          title: const Text('My profile'),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.phone)),
+      debugShowCheckedModeBanner: false,
+      home: ContactListPage(),
+    );
+  }
+}
+
+class ContactListPage extends StatelessWidget {
+  ContactListPage({super.key});
+
+  final List<Map<String, String>> contacts = [
+    {"name": "Ferdous", "phone": "01877-666666"},
+    {"name": "Ferdous", "phone": "01673-876547"},
+    {"name": "Hasan", "phone": "01745-777777"},
+    {"name": "Rabbi", "phone": "01745-709876"},
+    {"name": "Ahad", "phone": "01745-777777"},
+    {"name": "Hasan", "phone": "01745-777889"},
+    {"name": "Rahin", "phone": "01745-777777"},
+    {"name": "Hasan", "phone": "01745-777777"},
+    {"name": "Golam", "phone": "01745-777777"},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact List"),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Maynul",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "01745-456433",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey, // Button color
+                  foregroundColor: Colors.white, // Text color
+                ),
+                child: const Text("Add"),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: const Icon(
+                        Icons.person,
+                        size: 35,
+                        color: Colors.brown,
+                      ),
+
+                      title: Text(
+                        contacts[index]["name"]!,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      subtitle: Text(contacts[index]["phone"]!),
+
+                      trailing: const Icon(Icons.call, color: Colors.blue),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
-        ),
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 15),
-
-              CircleAvatar(
-                radius: 100,
-                child: Icon(Icons.icecream_outlined, size: 100),
-              ),
-
-              const SizedBox(height: 15),
-              const Text(
-                'Ice Cream is very delicious right?',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 60),
-
-              CircleAvatar(radius: 100, child: Icon(Icons.code, size: 100)),
-              const SizedBox(height: 15),
-              const Text(
-                'Programming is not boring if you love it',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
         ),
       ),
     );
